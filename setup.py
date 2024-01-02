b3b6d059ff30cfcecb21656a106ffd0556257512
ChipMcCallahan <thisischipmccallahan@gmail.com>
2024-01-01 17:50:40 -0800

    update project to depend on cc_tools

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    # Needed to silence warnings (and to be a worthwhile package)
     name='hybrid_cc',
     url='https://github.com/ChipMcCallahan/HybridCC',
     author='Chip McCallahan',
     author_email='thisischipmccallahan@gmail.com',
-    # Needed to actually package something
     packages=find_packages(where="src"),
     package_data={
         'hybrid_cc': [
@@ -16,14 +14,12 @@ setup(
         ]
     },
     package_dir={"": "src"},
-    # Needed for dependencies
     install_requires=[
+        'setuptools==67.6.0',
+        'cc_tools @ git+https://github.com/ChipMcCallahan/CCTools.git'
     ],
-    # *strongly* suggested for sharing
     version='0.1',
-    # The license can be anything you like
     license='LICENSE',
     description='Hybrid ruleset for playing certain tile-based games.',
-    # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
 )
