fb24eaa398127ef7d0d377193d5fe004a9bdfc1a
Chip McCallahan <thisischipmccallahan@gmail.com>
2024-01-14 17:52:12 -0800

    more boilerplate for elems

--- src/hybrid_cc/game/elements/instances/flippers.py
+++ src/hybrid_cc/game/elements/instances/flippers.py
@@ -3,5 +3,47 @@ from hybrid_cc.shared import Id
 
 
 class Flippers(Elem):
+    instances = {}
+
     def __init__(self):
         super().__init__(Id.FLIPPERS)
+
+    # --------------------------------------------------------------------------
+    # PLANNING PHASE
+    # --------------------------------------------------------------------------
+
+    def do_planning(self):
+        raise NotImplementedError("Implement or remove.")
+
+    # --------------------------------------------------------------------------
+    # INSTANCE BOOKKEEPING
+    # --------------------------------------------------------------------------
+    def construct_at(self, position):
+        raise NotImplementedError("Implement or remove.")
+
+    def destruct_at(self, position):
+        raise NotImplementedError("Implement or remove.")
+
+    def clone(self):
+        raise NotImplementedError("Implement or remove.")
+
+    # --------------------------------------------------------------------------
+    # ACCESS RULES
+    # --------------------------------------------------------------------------
+    def test_enter(self, position, other, direction):
+        raise NotImplementedError("Implement or remove.")
+
+    def test_exit(self, position, other, direction):
+        raise NotImplementedError("Implement or remove.")
+
+    def start_enter(self, position, other, direction):
+        raise NotImplementedError("Implement or remove.")
+
+    def start_exit(self, position, other, direction):
+        raise NotImplementedError("Implement or remove.")
+
+    def finish_exit(self, position, other, direction):
+        raise NotImplementedError("Implement or remove.")
+
+    def finish_enter(self, position, other, direction):
+        raise NotImplementedError("Implement or remove.")
