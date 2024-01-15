e20fdb426f066f3e84977cee0aba75b256f2e8ef
Chip McCallahan <thisischipmccallahan@gmail.com>
2024-01-15 12:24:00 -0800

    add utility Kwargs class with filter & conversion to/from tuple

--- src/hybrid_cc/shared/kwargs.py
+++ src/hybrid_cc/shared/kwargs.py
@@ -4,3 +4,17 @@ COLOR = "color"
 COUNT = "count"
 CHANNEL = "channel"
 SIDES = "sides"
+
+
+class Kwargs:
+    @staticmethod
+    def filter(_filter, **kwargs):
+        return {k: v for k, v in kwargs.items() if k in _filter}
+
+    @staticmethod
+    def to_tuple(**kwargs):
+        return tuple(sorted(kwargs.items()))
+
+    @staticmethod
+    def from_tuple(_tuple):
+        return {k: v for k, v in _tuple}
